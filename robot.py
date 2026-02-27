60f85b5da55be9d9e1684b40ca302a007b26514a
_ziyiyap <yapziyi13@gmail.com>
2026-02-27 09:41:08 +0800

    Battery consumption + distance tracker

--- robot.py
+++ robot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import random
 import seaborn as sns
 import time
-target = np.array([[100],[200]])
+target = np.array([[100],[20000]])
 class Robot:
     def __init__(self, blvl, temp, x,y,totald,malfc,rechargec):
         self.battery_level = blvl
@@ -15,6 +15,8 @@ class Robot:
         self.malf_count = malfc
         self.recharge_count = rechargec
     def move(self):
+        battery_cost_p_meter = 100 / 500
+        td = []
         global target
         position = np.array([[self.x_position],[self.y_position]])
         steps = 1000
@@ -26,9 +28,19 @@ class Robot:
             step_size = np.linalg.norm(remaining) * 0.1
             unit_vector = remaining / np.linalg.norm(remaining)
             new_position = position + noise + step_size * unit_vector
+
+            delta_position = new_position - position
+            dposition_mag = np.linalg.norm(delta_position)
+            td.append(dposition_mag)
+            battery_used = battery_cost_p_meter * dposition_mag
+            self.battery_level = self.battery_level - battery_used
             position = new_position
+            print(self.battery_level)
+            if self.battery_level <=0:
+                return 'Battery out'
             if np.linalg.norm(remaining)<0.1:
-                return f"Reached \n{np.round(new_position).astype('int')}! Steps taken: {i+1}"
+                self.total_distance = sum(td)
+                return f"Reached \n{np.round(new_position,2).astype('int')}! Steps taken: {i+1}\n Total distance:{self.total_distance}\nDisplacement:{np.linalg.norm(target)}"
         return 'Not enough steps!'
 
 jarvis = Robot(100, 30.0, 0, 0, 0.0, 0, 0)
