77ec16d78d82d3f164a37bdc2cbe23103445ed6f
_ziyiyap <yapziyi13@gmail.com>
2026-03-01 00:49:34 +0800

    Bug fixes, 'run simulation' state, fixed user_interface wont crash upon no simulation

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import analytics
-from robot import Robot
 import os
 import time
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-from simulation import Simulation
 from pathlib import Path
+from robot import Robot
+from simulation import Simulation
 
 #set base dir to this folder
 base_dir = Path(__file__).parent
@@ -29,72 +29,84 @@ class Login:
                     self.robot_data = json.load(f)
                 for i,data in enumerate(self.robot_data):
                     print(f"{i+1}. {data['name']}")
-                model = str(input("THERE EXISTS SOME MODELS. CHOOSE YOUR MODEL.\n")).strip().lower()
+                self.model = str(input("THERE EXISTS SOME MODELS. CHOOSE YOUR MODEL.\n")).strip().lower()
                 for i,data in enumerate(self.robot_data):
-                    if model in [str(i+1), data['name']]:
+                    if self.model in [str(i+1), data['name']]:
                         print(f"LOGGING INTO {data['name']}")
                         time.sleep(2)
+                        #unfinished
                 self.user_state = 'user_interface'
+
     def login(self):
         if self.user_state == 'add_model':
-            robot_name = str(input("ENTER ROBOT NAME\n"))
+            self.robot_name = str(input("ENTER ROBOT NAME\n"))
             try:
                 self.user_target = str(input('ENTER TARGET LOCATION:\n USAGE: <X_COORD>, <Y_COORD>\n')).strip().split(',')
-                self.user_target = (int(self.user_target[0]), int(self.user_target[1]))
-                user_robot = Robot(robot_name, 100, 26.0, 0, 0,self.user_target)
+                self.user_target = np.array([[int(self.user_target[0]), int(self.user_target[1])]])
+                self.user_robot = Robot(self.robot_name, 100, 26.0, 0, 0,self.user_target)
                 json_input = [{
-                    'name' : user_robot.name,
-                    'target' : user_robot.target.tolist(),
-                    'position' : user_robot.position.tolist()
+                    'name' : self.user_robot.name,
+                    'target' : self.user_robot.target.tolist(),
+                    'position' : self.user_robot.position.tolist()
                 }]
                 with open(robots_json,'w',encoding='utf-8') as file:
                     json.dump(json_input, file)
-                print(f"WELL DONE. {user_robot.name} HAS BEEN CREATED.")
+                print(f"WELL DONE. {self.user_robot.name} HAS BEEN CREATED.")
+                self.user_state = 'run_simulation'
+                return self.user_state,self.user_robot
             except ValueError or IndexError:
                 print('ERROR, PLEASE TRY AGAIN')
                 time.sleep(1)
                 os.system('cls')
                 return
-            #SIMULATOR
-            user_simulation = Simulation(user_robot)
-            user_simulation.run()
-
-            #GET DATAFRAME
-            user_dataframe = user_simulation.to_dataframe() 
-            time.sleep(2)
-            self.user_state = 'user_interface'
-            return self.user_state
+        
         elif self.user_state == 'user_interface':
             os.system('cls')
-            user_input = str(input("1. Robot Performance Report\n2. Graphs\n")).lower().strip()
-            if user_input in ['1','run robot']:
+            self.user_input = str(input("1. Robot Performance Report\n2. Graphs\n3. Run Simulation\n")).lower().strip()
+            if self.user_input in ['1','run robot']:
                 os.system('cls')
-                #report
-                print(f"{'='*5} ROBOT PERFORMANCE REPORT {'='*5}")
-                print(f"{analytics.total_distance(user_dataframe)}\n{analytics.average_temperature(user_dataframe)}\n{analytics.malfunction_count(user_dataframe)}\n{analytics.battery_efficiency(user_dataframe)}\n{analytics.longest_stable_run(user_dataframe)}")
-                print('='*36)
-                s = input('')
-            elif user_input in ['2','graphs']:
+                #check if robot ran any simulations
+                if self.user_simulation.count == 0:
+                    print(f"{self.user_robot.name} HAS NOT RUN ANY SIMULATIONS.\nROBOT PERFORMANCE REPORT CANNOT BE MADE.")
+                    time.sleep(2)
+                    return
+                else:
+                    #report
+                    print(f"{'='*5} ROBOT PERFORMANCE REPORT {'='*5}")
+                    print(f"{analytics.total_distance(self.user_dataframe)}\n{analytics.average_temperature(self.user_dataframe)}\n{analytics.malfunction_count(self.user_dataframe)}\n{analytics.battery_efficiency(self.user_dataframe)}\n{analytics.longest_stable_run(self.user_dataframe)}")
+                    print('='*36)
+                    _ = input('')
+            elif self.user_input in ['2','graphs']:
                 graph_dict = {
-                    '1' : (lambda: plt.plot(user_dataframe['tick'],user_dataframe['battery']), 'Tick', 'Battery Level','Battery Level against Ticks'),
-                    '2' : (lambda: plt.plot(user_dataframe['tick'],user_dataframe['temperature']), 'Tick', 'Temperature', 'Temperature against Ticks'),
-                    '3' : (lambda: plt.bar(user_dataframe.groupby('chunk')['malfunction'].sum().index, user_dataframe.groupby('chunk')['malfunction'].sum().values), 'Chunk', 'Malfunction','Malfunctions per Chunk'),
-                    '4' : (lambda: plt.scatter(user_dataframe['x'], user_dataframe['y']), 'X', 'Y','2D Robot Path')
+                    '1' : (lambda: plt.plot(self.user_dataframe['tick'],self.user_dataframe['battery']), 'Tick', 'Battery Level','Battery Level against Ticks'),
+                    '2' : (lambda: plt.plot(self.user_dataframe['tick'],self.user_dataframe['temperature']), 'Tick', 'Temperature', 'Temperature against Ticks'),
+                    '3' : (lambda: plt.bar(self.user_dataframe.groupby('chunk')['malfunction'].sum().index, self.user_dataframe.groupby('chunk')['malfunction'].sum().values), 'Chunk', 'Malfunction','Malfunctions per Chunk'),
+                    '4' : (lambda: plt.scatter(self.user_dataframe['x'], self.user_dataframe['y']), 'X', 'Y','2D Robot Path')
                 }
                 os.system('cls')
-                graph_input = str(input("1. Battery Level against Ticks\n2. Temperature against Ticks\n3. Malfunctions per Chunk\n4. Robot Path\n"))
-                if graph_input in graph_dict.keys():
+                self.graph_input = str(input("1. Battery Level against Ticks\n2. Temperature against Ticks\n3. Malfunctions per Chunk\n4. Robot Path\n"))
+                if self.graph_input in graph_dict.keys():
                     plt.figure()
-                    graph_dict[graph_input][0]()
-                    plt.xlabel(graph_dict[graph_input][1])
-                    plt.ylabel(graph_dict[graph_input][2])
-                    plt.title(graph_dict[graph_input][3])
+                    graph_dict[self.graph_input][0]()
+                    plt.xlabel(graph_dict[self.graph_input][1])
+                    plt.ylabel(graph_dict[self.graph_input][2])
+                    plt.title(graph_dict[self.graph_input][3])
                     plt.grid(True)
                     plt.show()
                 else:
                     print('Invalid')
                     time.sleep(1)
                 return
+        elif self.user_state == 'run_simulation':
+            os.system('cls')
+            self.user_simulation = Simulation(self.user_robot)
+            self.user_simulation.run()
+
+            #GET DATAFRAME
+            self.user_dataframe = self.user_simulation.to_dataframe() 
+            time.sleep(2)
+            self.user_state = 'user_interface'
+            return self.user_state
         
 
 user = Login()
--- simulation.py
+++ simulation.py
@@ -1,11 +1,12 @@
 import pandas as pd
-import robot as r
 
 class Simulation:
     def __init__(self,robot):
         self.robot = robot
+        self.count = 0
         self.log_data = []
     def run(self):
+        self.count +=1
         while self.robot.state != 'DONE': #each loop, 1 tick
             self.log_data.append(self.robot.update())
         return 'DONE'
